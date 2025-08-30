ebffbb3bc5af31f58600b9d8a794225dadc118cc
aftab-up <aftab.mathproblems@gmail.com>
2025-08-30 12:33:27 +0530

    Some chang

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
@@ -8,3 +14,8 @@ def hello():
 @app.get("/about")
 def about():
     return {"message": "This is a simple about application."}
+
+@app.get("/view")
+def view_data():
+    data = load_data()
+    return data
